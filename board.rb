60a685d0fc36f409f1f4822587d818d906fac151
Laurent Saidah <lsaidah@gmail.com>
2020-05-15 12:01:09 -0500

    Populating board with random Bombs

--- board.rb
+++ board.rb
@@ -2,7 +2,16 @@ require_relative "tile"
 require_relative 'player'
 class Board
 
-    def initialize(size = 9)
-        @grid = Array.new(9){Array.new(9, Tile.new())}
+    attr_reader :grid
+    def initialize(size = 9, level = 1)
+        @grid = Array.new(size){Array.new(size, Tile.new())}
+        populate(size*level)
+    end
+
+    def populate(level)
+        level.times {@grid[rand(0...@grid.length)][rand(0...@grid.length)] = "B"}
     end
 end
+
+b = Board.new
+p b.grid
