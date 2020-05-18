6638f8d1c2f6557483ca738794849867d0438fc6
Laurent Saidah <lsaidah@gmail.com>
2020-05-18 11:15:42 -0500

    added a flagged boolean

--- tile.rb
+++ tile.rb
@@ -1,13 +1,14 @@
 class Tile
 
-    attr_accessor :val, :hidden
+    attr_accessor :val, :hidden, :flagged
     def initialize(val = nil)
         @val = val
         @hidden = true
+        @flagged = false
     end
 
     def to_s
-        hidden? ? (print '*') : (print @val)
+        hidden? ? (print ' ') : (print @val)
     end
 
     def hidden?
