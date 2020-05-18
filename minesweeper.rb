50336dca48ff81ca194fb378c2b10578a2a1fd12
Laurent Saidah <lsaidah@gmail.com>
2020-05-18 11:49:56 -0500

    started a fringe_tiels method and added a valid_pos? method

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,4 +1,32 @@
-
+require_relative 'board'
+require_relative 'tile'
+require_relative 'player'
 class Minesweeper
 
+    def initialize
+        @board = Board.new
+    end
+
+    def reveal(pos)
+        @board.reveal_tile(pos)
+    end
+
+    def fringe_tiles(pos)
+        finge = []
+        x, y = pos
+        
+    end
+
+    def valid_pos?(pos)
+        x, y = pos
+        if x < 0 || x >= @board.grid.length
+            return false
+        elsif y < 0 || y >= @board.grid.length
+            return false            
+        end 
+        true
+    end 
 end
+
+m = Minesweeper.new
+p m.valid_pos?([-1, 2])
