fd1bae15620290bb3f8e80a3515730e20363aecd
Laurent Saidah <lsaidah@gmail.com>
2020-05-18 14:24:31 -0500

    changing the reveal method and workign on run method

--- minesweeper.rb
+++ minesweeper.rb
@@ -10,8 +10,16 @@ class Minesweeper
 
     def reveal(pos)
         @board.reveal_tile(pos)
-        @game_over if @board[pos].bomb?
-        @board.fringe_tiles(pos).each {|tile| @board.reveal_tile(tile)}
+        @game_over = true if @board[pos].bomb?
+        # if @board[pos].val = "0"
+        #     @board.fringe_tiles(pos).each do |fringe_tile| 
+        #         if @board[fringe_tile].val = "0"
+        #             self.reveal(fringe_tile)
+        #         elsif !@board[fringe_tile].bomb? || !@board[fringe_tile.flagged]
+        #             @board.reveal_tile(fringe_tile)
+        #         end
+        #     end
+        # end
     end
 
     def render()
@@ -28,8 +36,12 @@ class Minesweeper
 
     def run
         player = Player.new("Laurent")
+        @board.show_board
+        p "-----"
         until @game_over
              @board.render
+           
+            
             pos = player.get_pos
             if @board.valid_pos?(pos)
                 reveal(pos)
@@ -37,7 +49,14 @@ class Minesweeper
                 "That was not a valid move"
                 pos = player.get_pos
             end
+            if @board[pos].val = "0"
+                fringe = @board.fringe_tiles(pos).select {|fringe_tile| fringe_tile if !@board[fringe_tile].bomb? || !@board[fringe_tile].flagged?}
+                debugger
+                fringe.each {|tile| reveal(tile)}
+            end
         end
+        p "Game over!"
+        @board.show_bombs
     end
 
 end
