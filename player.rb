22d4e840ec3f22cc1dd62ed728c270e30f0ad880
Laurent Saidah <lsaidah@gmail.com>
2020-05-18 13:37:19 -0500

    added a get_pos method

--- player.rb
+++ player.rb
@@ -5,6 +5,17 @@ class Player
         @name = name
     end
 
-    def get_move
+    def get_pos
+        print "#{@name} please enter a postion: "
+        pos = gets.chomp
+        parse_pos(pos)
+    end
+
+    def parse_pos(pos)
+        pos = pos.split(",").map!{|int| Integer(int)}
     end
 end
+
+# p = Player.new("Laurent")
+# pos = p.get_pos
+# p pos
