7ab9fd5dfdfc49c24ca24e4a69a9aa9e639990f1
Laurent Saidah <lsaidah@gmail.com>
2020-05-18 13:36:58 -0500

    added a run method

--- minesweeper.rb
+++ minesweeper.rb
@@ -5,10 +5,12 @@ class Minesweeper
 
     def initialize
         @board = Board.new
+        @game_over = false
     end
 
     def reveal(pos)
         @board.reveal_tile(pos)
+        @game_over if @board[pos].bomb?
         @board.fringe_tiles(pos).each {|tile| @board.reveal_tile(tile)}
     end
 
@@ -19,14 +21,27 @@ class Minesweeper
     def show_bombs
         @board.show_bombs
     end
+
+    def game_over?
+        @game_over
+    end
+
+    def run
+        player = Player.new("Laurent")
+        until @game_over
+             @board.render
+            pos = player.get_pos
+            if @board.valid_pos?(pos)
+                reveal(pos)
+            else
+                "That was not a valid move"
+                pos = player.get_pos
+            end
+        end
+    end
+
 end
 
 m = Minesweeper.new
-m.render
-p "----"
-m.reveal([2,2])
-
-m.render
-p "++++"
-m.show_bombs
+m.run
 
